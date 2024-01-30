4e75d759ae0337e6b8e25e9826c293338579f3e2
leonardodefendi <leo_defendi@hotmail.com>
2024-01-30 17:20:22 -0300

    Finaliza requisito 6 model e migration users

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,54 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeUsers extends
+  Model<InferAttributes<SequelizeUsers>,
+  InferCreationAttributes<SequelizeUsers>> {
+  declare id: CreationOptional<number>;
+
+  declare username: string;
+
+  declare role: string;
+
+  declare email: string;
+
+  declare password: string;
+}
+
+SequelizeUsers.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default SequelizeUsers;
