f3756a17146db782d71a04f3bde4ed9730a747ae
leonardodefendi <leo_defendi@hotmail.com>
2024-01-30 15:08:40 -0300

    Finaliza requisito 01

---
+++ app/backend/src/database/migrations/01-create-teams.ts
@@ -0,0 +1,23 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import {Team} from '../../Interfaces/teamsType'
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<{id: number, team_name: string}>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      team_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'team_name'
+      }
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+};
---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,36 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeTeam extends
+  Model<InferAttributes<SequelizeTeam>,
+  InferCreationAttributes<SequelizeTeam>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+SequelizeTeam.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default SequelizeTeam;
